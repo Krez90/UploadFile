6b48fa8e12c8459a7c9729191b438f02beee233b
Krez90 <bilel.hamimed@yahoo.fr>
2019-02-27 13:28:40 +0100

    maj27/02/2019

--- 404.php
+++ 404.php
@@ -5,24 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/page404.css">
     <title>Page 404</title>
-    <link rel="stylesheet" href="page404.css">
+    
 </head>
 
 <body>
-    <section>
-        <h1>Oups !<br><br>
-         On dirait bien qu’on c’est égaré <br><br>Monsieur Soleil n’est pas très content de cette visite improvisée.<br><br>
-         Page 404 !</h1>
-        <br>
-        <img src="images/soleil.png" style="max-width: 500px">
-        <br>
+    <section class="container">
+        <div class="row">
+            <div class="col" id="titre">
+                <h1>Oups !</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm" id="texte">
+                <p>On dirait bien qu’on s'est égaré<br>Monsieur Soleil n’est pas très content de cette visite improvisée...<br>
+               </p><br>
+            </div>
+            <div class="col-sm" id="img_div_soleil">
+                <img src="public/images/soleil.png" class="img_soleil">
+            </div>
+        </div>
     </section>
-    <footer>
-    
-    <div class="nuage404"><img src="images/nuages4042.png"></div>
+
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col" id="footer">
+            <img src="public/images/nuages4042.png" class="nuage404">
+        </div>
+    </div>
 </footer>
 
-</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
+</body>
 </html>
--- index.php
+++ index.php
@@ -2,63 +2,60 @@
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>UploadIt</title>
+  <title>Uploadoit</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="public/css/nunu.css">
   <!-- Lien GOOGLE FONT typo Raleway. -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <!-- Lien CSS FILEPOND !!IMPORTANT!!. -->
   <link href="public/filepond-master/dist/filepond.css" rel="stylesheet">
+  
 </head>
 <body>
 
-  <header>
-    <div id="logo" class="padding">
-    <a id="center" href="https://thecleaureliea.promo-24.codeur.online/UploadIt/" >
-    <img class="left" src="public/images/logo2.png">
-    <!--  <span>UploadIt</span> -->
-    </a>
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col" id="logo2">
+        <a href="//bilelh.promo-24.codeur.online/UploadIt/">
+          <img class="logo_header" src="public/images/logo2.png"></a>
+      </div>
+    </div>
   </div>
-  </header>
+</header>
 
-  <section class="padding">
-  <div id="box" class="taille">
-      <div id="center">
-    <form  id = "contact" method="post" action="formulaire.php" enctype="multipart/form-data">
-      
-      <label></label><input name="destinataire" id="destinataire" placeholder="Email du destinataire" required><span id="missDest"></span>
-      <p class="errorMessage"></p>
-      <br/>
-      <br/>
-      <label></label><input name="expediteur" id="expediteur" placeholder="Email de l'expéditeur" required><span id="missExp"></span>
-      <p class="errorMessage"></p>
-      <br/>
-      <br/>
-      <label></label><textarea name="message" id="message" cols="30" rows="5" placeholder="Message"></textarea>
-      <p class="errorMessage"></p>
-      <br/>
-      <br/>
-      <!-- Input FILEPOND. -->
-      <input type="file"
-    class="filepond"
-    name="fichier"
-    multiple 
-    data-max-file-size="3MB"
-    data-max-files="3">
-      <button type="submit" name="envoyer" id="envoyer" value="enregister">Envoyer</button>
+<div class="container">
+  <div class="row">
+    <div class="col" id="formulaire">
+      <form  id="contact" method="post" action="formulaire.php" enctype="multipart/form-data">
+        <input name="destinataire" id="destinataire" placeholder="Email du destinataire" required><br><span id="missDest"></span>
+        <p class="errorMessage"></p>
+        
+        <input name="expediteur" id="expediteur" placeholder="Email de l'expéditeur" required><br><span id="missExp"></span>
+        <p class="errorMessage"></p>
+        
+        <textarea name="message" id="message" cols="30" rows="5" placeholder="Message"></textarea><br>
+        <p class="errorMessage"></p>
+<!-------------------------------------------------- Input FILEPOND. ------------------------------------------------>
+<div class="filepond_js">
+  <input type="file" class="filepond" name="fichier" multiple data-max-file-size="3MB" data-max-files="3">
+</div>
+    <button type="submit" name="envoyer" id="envoyer" value="enregister">Envoyer</button>
     </form>
-    
   </div>
-
-  <div class="nuage">
-  <img id="taille_nunu" src="public/images/nunu.png">
   </div>
-  
+  <div class="row">
+    <div class="col" id="div_nunu">
+      <img id="taille_nunu" src="public/images/nunu.png">
+    </div>
   </div>
-  </section>
+</div>
 
-
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   <!-- Chemin vers les fichier de références FILEPOND !!IMPORTANT!!. -->
   <script src="public/filepond-master/dist/filepond.js"></script>
   <!-- Script js FILEPOND. -->
